d0fd4ad2a06bc084f015d2c5d619c376f192db11
LividWebWatcherAI <60311378+LividWebWatcherAI@users.noreply.github.com>
2020-01-26 03:50:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ function App() {
     <div className="App">   
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     
+         <p>
+          <code>src/App.js</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+        </a>
      
       </header>
    
- <h1>Welcome</h1>
+ 
     </div>
   );
 }
